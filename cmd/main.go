bc29c64da1ac48c1560f7406a2d397198ef543f8
Prashant Patel <prashant.n.patel@ibm.com>
2020-06-04 23:39:10 -0700

    Fixing the code to add file

--- cmd/main.go
+++ cmd/main.go
@@ -2,24 +2,16 @@ package main
 
 import (
 	"fmt"
+	"github.com/prashant182/filetree/pkg/tree"
+	"gopkg.in/yaml.v2"
 	"io/ioutil"
 	"os"
 	"path/filepath"
-
 	"github.com/sirupsen/logrus"
-	"gopkg.in/yaml.v2"
 )
 
 func main() {
-	IteratePath("/Users/prashant/go/src/github.com/prashant182/filetree/test")
-}
-
-//File is the type of structure which holds the key information regarding the Files on a disk
-type File struct {
-	Name     string
-	Path     string
-	IsDir    bool
-	Children []*File
+	IteratePath("/Users/prashant/go/src/github.com/prashant182/filetree/sc")
 }
 
 //IteratePath iterates over the files in a given directory and creates a JSON/Yaml structure out of it.
@@ -29,19 +21,20 @@ func IteratePath(path string) {
 		logrus.Error("Path : ", path, " doesn't exist. ", err)
 	}
 	rootfile := ToFile(rootFile, path)
-	stack := []*File{rootfile}
+	stack := []*tree.Node{rootfile}
 	for len(stack) > 0 {
 		file := stack[len(stack)-1]
 		stack = stack[:len(stack)-1]
 		children, _ := ioutil.ReadDir(file.Path)
 		for _, chld := range children {
-			child := ToFile(chld, filepath.Join(file.Path, chld.Name())) //turn it into a File object
-			file.Children = append(file.Children, child)                 //append it to the children of the current file popped
+			child := ToFile(chld, filepath.Join(file.Path, chld.Name()))
+			file.Children = append(file.Children, child)
 			stack = append(stack, child)
 		}
 	}
 
-	output, err := yaml.Marshal(rootfile)
+	m := rootfile.ToMap()
+	output, err := yaml.Marshal(m)
 	if err != nil {
 		logrus.Error("YAML marshal error ", err)
 	}
@@ -49,13 +42,11 @@ func IteratePath(path string) {
 }
 
 //ToFile converts the fileInfo object into a File object for filetree package
-func ToFile(file os.FileInfo, path string) *File {
-	outFile := File{
+func ToFile(file os.FileInfo, path string) *tree.Node {
+	outFile := tree.Node{
 		Name:     file.Name(),
-		IsDir:    file.IsDir(),
-		Children: []*File{},
+		Children: []*tree.Node{},
 		Path:     path,
 	}
-
 	return &outFile
 }
